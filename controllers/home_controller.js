f65e35d072b054a43565aaaa51a86912f5779c3a
Kunal Khairnar <you@example.comkunal.khairnar96@gmail.com>
2020-09-10 16:14:50 +0530

    find the neerest car

---
+++ controllers/home_controller.js
@@ -0,0 +1,5 @@
+
+
+module.exports.home = function(req,res){
+    return res.end("Welcome to my cab service")
+}
---
+++ index.js
@@ -0,0 +1,29 @@
+const express= require('express');
+const path= require('path');
+const port= 8000;
+const cookieParser= require('cookie-parser');
+var app = express();
+app.use(express.urlencoded());
+const expressLayout=require('express-ejs-layouts');
+app.use(cookieParser());
+const boduparser= require('body-parser');
+const ejs= require('ejs');
+const session= require('express-session');
+app.use(express.static('./assets'));
+app.set('view engine','ejs');
+app.set('views','./views');
+app.use(expressLayout);
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
+app.use('/',require('./routes/index'))
+
+
+
+
+app.listen(port, function(err){
+    if(err){
+        console.log('Error in connnecting to port ',port );
+    }
+    console.log('Connected to port no:',port)
+})
